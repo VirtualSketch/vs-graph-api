2db1650b3f83a2bc4c1a8d193222c90ad1aed7ce
Leonardo-sm <leonardo.smichelutti@gmail.com>
2022-08-18 21:31:28 -0300

    ✨ Add account route

--- app/models/account.py
+++ app/models/account.py
@@ -1,17 +1,20 @@
-from base_model import BaseModel
+from .base_model import BaseModel
 
 class Account(BaseModel):
-    def __init__(self, session_id, raw_account):
+    def __init__(self, raw_account) -> None:
+        session_id = ''
         super().__init__(session_id, raw_account)
     
     _graph_params_schema = {
         "type": "object",
         "properties": {
-            "session_id": {"type": "string"},
             "raw_account": {"type": "string"},
             },
-        "required": ["session_id", "raw_account"]
+        "required": ["raw_account"]
     }
+
+    def get_resolved_account(self):
+        return str('x + y')
 
     def validate_params(self, json_data):
         return super().validate_params(json_data, self._graph_params_schema)
