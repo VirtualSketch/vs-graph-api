b694a64dd45e110fb0c7a1a9407b35476bc7c27a
Leonardo-sm <leonardo.smichelutti@gmail.com>
2022-08-22 19:53:39 -0300

    ✨ Add graph generator function in get_graph method

--- app/controllers/graph_controller.py
+++ app/controllers/graph_controller.py
@@ -5,8 +5,9 @@ from ..models.graph import Graph
 def graph_controller(params):
     raw_expression = params.get('raw_expression')
     session_id = params.get('session_id')
+    graph_color = params.get('graph_color')
 
-    graph = Graph(raw_expression=raw_expression, session_id=session_id)
+    graph = Graph(raw_expression=raw_expression, session_id=session_id, graph_color=graph_color)
 
     try:
         graph.validate_params(params)
--- app/models/graph.py
+++ app/models/graph.py
@@ -1,22 +1,27 @@
 import os
+from pathlib import Path
 from app.utils.get_image_url import get_image_url
 from app.utils.get_random_string import get_random_string
 
+from app.scripts.create_cartesian_graph import simple_degree_graph
 from app.utils.get_static_path import get_static_path
-from pathlib import Path
 from .base_model import BaseModel
 
 class Graph(BaseModel):
-    def __init__(self, session_id, raw_expression) -> None:
+    def __init__(self, session_id, raw_expression, graph_color) -> None:
+        self.graph_color = graph_color
         super().__init__(session_id, raw_expression)
-    
+
+
+    image_path = ''
     _graph_params_schema = {
         "type": "object",
         "properties": {
             "session_id": {"type": "string"},
             "raw_expression": {"type": "string"},
+            "graph_color": {"type": "string"},
             },
-        "required": ["session_id", "raw_expression"]
+        "required": ["session_id", "raw_expression", "graph_color"]
     }
 
     def get_graph(self):
@@ -28,13 +33,16 @@ class Graph(BaseModel):
             os.mkdir(static_path)
 
         if not is_exists_graph_dir:
-            image_path = os.path.join(static_path, self.session_id)
-            os.mkdir(image_path)
+            self.image_path = os.path.join(static_path, self.session_id)
+            os.mkdir(self.image_path)
+        else:
+            self.image_path = os.path.join(static_path, self.session_id)
 
         graph_name = get_random_string(8)
-        image_url = get_image_url(session_id=self.session_id, file_name=graph_name)
+        image_url = f'{get_image_url(session_id=self.session_id, file_name=graph_name)}.png'
 
-        print('call generate graph function')
+        simple_degree_graph(fileName=graph_name, equation=self.raw_expression, path=self.image_path, color=self.graph_color)
+
         return image_url
 
     def validate_params(self, json_data):
