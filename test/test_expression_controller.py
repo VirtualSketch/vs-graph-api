871ccc66828d1a4cf3234e21c1aa3c684acff667
MariaBeatrizDS <biameupessoal@gmail.com>
2022-09-13 19:10:50 -0300

    ✅ tests with the comments

--- test/test_expression_controller.py
+++ test/test_expression_controller.py
@@ -6,43 +6,72 @@ from app.controllers.expression_controller  import expression_controller
     [
         ({"raw_expression": "- 2 * x + 3"}, 
         [
+        "f(x) = - 2 * x + 3 = - 2 * (-3) + 3 = 9",
+        "f(x) = - 2 * x + 3 = - 2 * (-2) + 3 = 7",
+        "f(x) = - 2 * x + 3 = - 2 * (-1) + 3 = 5",
+        "f(x) = - 2 * x + 3 = - 2 * (0) + 3 = 3",
+        "f(x) = - 2 * x + 3 = - 2 * (1) + 3 = 1",
+        "f(x) = - 2 * x + 3 = - 2 * (2) + 3 = -1",
+        "f(x) = - 2 * x + 3 = - 2 * (3) + 3 = -3"
         ]),
         ({"raw_expression": "x ** 2 + 4"}, 
         [
-        ]),
+        "f(x) = x ** 2 + 4 = (-3) ** 2 + 4 = 13",
+        "f(x) = x ** 2 + 4 = (-2) ** 2 + 4 = 8",
+        "f(x) = x ** 2 + 4 = (-1) ** 2 + 4 = 5",
+        "f(x) = x ** 2 + 4 = (0) ** 2 + 4 = 4",
+        "f(x) = x ** 2 + 4 = (1) ** 2 + 4 = 5",
+        "f(x) = x ** 2 + 4 = (2) ** 2 + 4 = 8",
+        "f(x) = x ** 2 + 4 = (3) ** 2 + 4 = 13"
+         ]),
         ({"raw_expression": "x ** 2 - 2 * x - 3"}, 
         [
+        "f(x) = x ** 2 - 2 * x - 3 = (-3) ** 2 - 2 * (-3) - 3 = 12",
+        "f(x) = x ** 2 - 2 * x - 3 = (-2) ** 2 - 2 * (-2) - 3 = 5",
+        "f(x) = x ** 2 - 2 * x - 3 = (-1) ** 2 - 2 * (-1) - 3 = 0",
+        "f(x) = x ** 2 - 2 * x - 3 = (0) ** 2 - 2 * (0) - 3 = -3",
+        "f(x) = x ** 2 - 2 * x - 3 = (1) ** 2 - 2 * (1) - 3 = -4",
+        "f(x) = x ** 2 - 2 * x - 3 = (2) ** 2 - 2 * (2) - 3 = -3",
+        "f(x) = x ** 2 - 2 * x - 3 = (3) ** 2 - 2 * (3) - 3 = 0"
         ]),
         ({"raw_expression": "-x ** 2 + 4 * x - 3"}, 
         [
+        "f(x) = -x ** 2 + 4 * x - 3 = -(-3) ** 2 + 4 * (-3) - 3 = -24",
+        "f(x) = -x ** 2 + 4 * x - 3 = -(-2) ** 2 + 4 * (-2) - 3 = -15",
+        "f(x) = -x ** 2 + 4 * x - 3 = -(-1) ** 2 + 4 * (-1) - 3 = -8",
+        "f(x) = -x ** 2 + 4 * x - 3 = -(0) ** 2 + 4 * (0) - 3 = -3",
+        "f(x) = -x ** 2 + 4 * x - 3 = -(1) ** 2 + 4 * (1) - 3 = 0",
+        "f(x) = -x ** 2 + 4 * x - 3 = -(2) ** 2 + 4 * (2) - 3 = 1",
+        "f(x) = -x ** 2 + 4 * x - 3 = -(3) ** 2 + 4 * (3) - 3 = 0"
         ])
     ])
-#Defining the test
+
+#Test definition
 def test_expression_controller(params, expected):
     #Sending the parameter to the function
     result = expression_controller(params)
-    #Comparing the result of the function with the expected value passed in the parametrize
-    assert result == expected
+    #If the function return a list proceed further
+    if(type(result) == list):
+        #If the result is equal to the expected, pass the test
+        assert result == expected
+    else:
+        assert False
 
 #Passing the parameters to the test
-@pytest.mark.parametrize("errors", [({"raw": "- 2 * x + 3"}), ({"raw_expression": "- 2d - r"})])
+@pytest.mark.parametrize("errors", [({"raw": "- 2 * x + 3"}), ({"expression": "- 2 * x + 3"}), ({"raw_expression": "- 2d - r"}), ({"raw_expression": ""}) ])
 
-#Defining the test
+#Test definition
 def test_expression_controller_json_error(errors):
-    #Try to rum the function
-    #Passing the parameters to the function
+    #The function expression_controller needs to throw an error, otherwise fail the test
     try:
-        #Passing the parameter to the function
         expression_controller(errors)
-        #The functions in this test will throw a error, so if the function return something that is not a error, the test will fail
         assert False
+
     #Catch the exception and give a name to it
     except Exception as error:
-        #Transfoming the error into a string
         getErrorMessage = str(error)
+        #If it is one of the expected exceptions, print the error's message and pass the test
         if(getErrorMessage == '500 Internal Server Error: JSON Schema is not valid' or getErrorMessage == '500 Internal Server Error: Unknown error'):
-            #Print the error in the console if the message is equal to "500 Internal Server Error: JSON Schema is not valid"
             print(getErrorMessage)
-            #Pass the test
             assert True
             
--- test/test_routes.py
+++ test/test_routes.py
@@ -1,29 +1,94 @@
-from ast import Assert
+import base64
+from sre_compile import isstring
 import pytest
 import json
 import requests
 
-from app.routes import get_equation
+#Defining parameters of the test
+@pytest.mark.parametrize("equation", 
+[
+    ({"raw_expression": "- 2 * x + 3"}), 
+    ({"raw_expression": "- x ** 2 + 5 * x + 6",}), 
+    ({"raw_expression": "- 4 * x + 2",})
+])
 
-@pytest.mark.parametrize("equation", [({"raw_expression": "- 2 * x + 3"})])
-
+#Test definition
 def test_post_equation(equation):
+    #Doing a post request to the API
     url = "https://virtualsketch.herokuapp.com/api/get_equation"
     response = requests.post(url, json=equation)
     validResponse = json.loads(response.content)
+    #If the return from the API is a object and a list, pass the test
     if(isinstance(validResponse, object)):
         result = validResponse.get('equation')
-        print(result)
         assert type(result) == list
     else:
         assert False
 
-# @pytest.mark.parametrize("graph", [({"raw_expression": "x ** 2 - 4 * x - 12", "graph_color": "#fff"})])
+#Defining parameters of the test
+@pytest.mark.parametrize("graph", 
+[
+    ({"raw_expression": "x ** 2 - 4 * x - 12", "graph_color": "#fff"}), 
+    ({"raw_expression": "2 * x + 2", "graph_color": "#000"}),
+    ({"raw_expression": "2 * x + 4", "graph_color": "#fff"})
+])
 
-# def test_post_graph(graph):
-#     url = "https://virtualsketch.herokuapp.com/api/get_graph"
-#     response = requests.post(url, json=graph)
-#     validResponse = json.loads(response.content)
-#     print(response)
+#Test definition
+def test_post_graph(graph):
+    #Doing a post request to the API
+    url = "https://virtualsketch.herokuapp.com/api/get_graph"
+    response = requests.post(url, json=graph)
+    validResponse = json.loads(response.content)
+    content = validResponse.get('graph_base64_image')
     
+    #If the return of the API is a string and a base64, the test will pass
+    if(isstring(content)):
+        print("It is a base64")
+        assert base64.b64decode(content)
+    else:
+        print("Not a string and is not a base64.")
+        assert False
 
+#Defining parameters of the test
+@pytest.mark.parametrize("errors", 
+[
+    ({"raw_expression": "x ** 2 - 4 * x - 12", "graph": "#fff"}), 
+    ({"expression": "x ** 2 - 4 * x - 12", "graph_color": "#fff"}),
+    ({"raw": "x ** 2 - 4 * x - 12", "color": "#fff"}),
+    ({"raw_expression": "x ** 2 - 4 * x - 12", "graph_color": "%*"}),
+    ({"raw_expression": "k ** 2 - 4 * t - 12", "graph_color": "#fff"}),
+    ({"raw_expression": "k ** 2 - 4 * t - 12", "graph_color": "#/*"})
+])
+
+#Test definition
+def test_post_graph_error(errors):
+    #Doing a post request to the API
+    url = "https://virtualsketch.herokuapp.com/api/get_graph"
+    result = requests.post(url, json=errors)
+    #If the return from the API is error mesage that we programed, pass the test
+    if(result.content == b'500 Internal Server Error: JSON Schema is not valid' or result.content == b'500 Internal Server Error: Unknown error'):
+        assert True
+    else:
+        print("Is not a valid error.")
+        assert False
+
+#Defining parameters of the test
+@pytest.mark.parametrize("errors_equation", 
+[
+    ({"raw": "- 2 * x + 3"}), 
+    ({"expression": "- 2 * x + 3"}), 
+    ({"raw_expression": "- 2d - r"}), 
+    ({"raw_expression": ""}) 
+])
+
+#Test definition
+def test_post_expression_error(errors_equation):
+    #Doing a post request to the API
+    url = "https://virtualsketch.herokuapp.com/api/get_equation"
+    result = requests.post(url, json=errors_equation)
+    #If the return from the API is error mesage that we programed, pass the test
+    if(result.content == b'500 Internal Server Error: JSON Schema is not valid' or result.content == b'500 Internal Server Error: Unknown error'):
+        assert True
+    else:
+        print("Is not a valid error.")
+        assert False
