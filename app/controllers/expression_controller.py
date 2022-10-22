373e9e02dd340fd9c80c25ebd3e6e2b9c8d6aaf4
NickKlementiev <nklementiev@hotmail.com>
2022-10-22 12:21:18 -0300

    :sparkles: Adding 'x2' to expression treatment

--- app/controllers/expression_controller.py
+++ app/controllers/expression_controller.py
@@ -3,8 +3,16 @@ from flask import abort
 from app.errors.validate_json_error import ValidateJsonError
 from app.models.expression import Expression
 
+import re
+
 def expression_controller(params):
     raw_expression = params.get('raw_expression')
+
+    result = re.search(r"([a-z])(\d)", raw_expression)
+
+    if result is not None:
+        raw_expression = re.sub(r"[a-z]\d", result.group(1) + " ** " + result.group(2), raw_expression)
+
     expression = Expression(raw_expression=raw_expression)
 
     try:
