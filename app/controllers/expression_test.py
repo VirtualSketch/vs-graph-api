4b8db2c8c4d390423bcdf3b44cdd469c7a1c7b20
NickKlementiev <nklementiev@hotmail.com>
2022-11-02 21:44:55 -0300

    :wrench: Adding regex to graph controller

---
+++ app/controllers/expression_test.py
@@ -0,0 +1,45 @@
+from flask import abort
+
+from app.errors.validate_json_error import ValidateJsonError
+from app.models.expression import Expression
+
+import re
+
+def expression_controller(params):
+    print(params)
+
+    raw_expression = params.get('raw_expression')
+
+    print("Raw Expression:", raw_expression)
+
+    has_letter_after_digit = re.search(r"(\d)([a-z])", raw_expression)
+
+    if has_letter_after_digit is not None:
+        raw_expression = re.sub(r"\d[a-z]", has_letter_after_digit.group(1) + " * " + has_letter_after_digit.group(2), raw_expression)
+
+    result = re.search(r"([a-z])(\d)", raw_expression)
+
+    if result is not None:
+        raw_expression = re.sub(r"[a-z]\d", result.group(1) + " ** " + result.group(2), raw_expression)
+
+    print("After regex substitution:", raw_expression)
+
+    expression = Expression(raw_expression=raw_expression)
+
+    params = {'raw_expression': raw_expression}
+
+    try:
+        expression.validate_params(params)
+        equation = expression.get_equation()
+        return equation
+    except ValidateJsonError:
+        abort(500, 'JSON Schema is not valid')
+    except:
+        abort(500, 'Unknown error')
+
+
+# params = {"raw_expression": "x ** 2 + 5 * x - 6"}
+# expression_controller(params)
+params = {"raw_expression": "3x2+5x-6"}
+expression_controller(params)
+
